86016bad9c0310593b75bff124331d84b70bb3c7
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2021-09-14 22:27:17 +0530

    Time: 4 ms (60.60%), Space: 6.6 MB (12.88%) - LeetHub

--- length-of-last-word/length-of-last-word.cpp
+++ length-of-last-word/length-of-last-word.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        /*int i=s.length()-1;
-        int len=0;
-        while(s[i]==' ')
-            i--;
-        while(s[i]!=' ' && i>=0){
-            len++;
-            i--;
-        }
-        return len;*/
-        int res=0,i=s.size()-1;
+        int i=s.length()-1;
+        int res=0;
         while(s[i]==' ')
             i--;
         for(;i>=0&&s[i]!=' ';i--)
