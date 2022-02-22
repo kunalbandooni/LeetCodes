c53d0553619c9febea31315e33eac84bc02c094c
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-02-22 23:54:01 +0530

    Time: 4 ms (47.34%), Space: 5.9 MB (92.06%) - LeetHub

---
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int result = 0;
+        for (const auto& c : s){
+            result *= 26;
+            result += c  - 'A' + 1;
+        }
+        return result;
+    }
+};
