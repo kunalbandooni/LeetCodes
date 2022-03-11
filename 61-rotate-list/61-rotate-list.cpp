70fe8f88c5fbdd3632b7fa87e9c9ece47256fd15
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-03-11 15:16:50 +0530

    Time: 8 ms (73.85%), Space: 11.8 MB (55.87%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -11,53 +11,20 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        
-        if(!head){
-            return head;
+        if (head == nullptr) return head;
+        ListNode* p = head;
+        int len = 1;
+        while (p->next != nullptr) {
+            p = p->next;
+            len++;
         }
-        ListNode*temp=head;
-        int c=1;
-        //Find the size of the list
-        while(temp->next){
-            temp=temp->next;
-            c++;
+        p->next = head;
+        k = k % len;
+        for (int i = 0; i < len - k; i++) {
+            p = p->next;
         }
-        //connect for circular list
-        temp->next=head;
-        //remove cycle from k
-        k=c-k%c;
-        k--; //reduce by one as we need to reach prev elemnt of the new head
-        temp=head;
-        while(k--){
-            temp=temp->next;
-        }
-        head=temp->next;
-        temp->next=NULL;
+        head = p->next;
+        p->next = nullptr;
         return head;
-        /*if(!head)
-            return NULL;
-        ListNode* temp1=head;
-        int n=1;
-        while(temp1){
-            temp1=temp1->next;
-            n++;
-        }
-        k=k%n;
-        int z=n-k-1;
-        if(z==0)
-            return head;
-        ListNode *temp=head;
-        while(z!=0){
-            temp=temp->next;
-            z--;
-        }
-        ListNode* prev=temp;
-        while(temp->next!=NULL){
-            temp=temp->next;
-        }
-        temp->next=head;
-        head=prev->next;
-        prev->next=NULL;
-        return head;*/
     }
 };
