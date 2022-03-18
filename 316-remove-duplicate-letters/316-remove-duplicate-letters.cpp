e239d140f63530756922ced708866bd973b63d60
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-03-18 10:41:46 +0530

    Time: 4 ms (61.69%), Space: 6.9 MB (36.31%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
+        vector<int> dict(256, 0);
+        vector<bool> visited(256, false);
+        for(auto ch : s)  dict[ch]++;
+        string result = "0";
+        /** the key idea is to keep 
+            a monotically increasing sequence **/
+        for(auto c : s) {
+            dict[c]--;
+            /** to filter the previously visited elements **/
+            if(visited[c])  continue;
+            while(c < result.back() && dict[result.back()]) {
+                visited[result.back()] = false;
+                result.pop_back();
+            }
+            result += c;
+            visited[c] = true;
+        }
+        return result.substr(1);
+        /*
         string res="";
         int lastPos[26] = {}; // the last position of the char
         bool added[26] = {};  // if the char is added to the result
@@ -20,5 +39,6 @@ public:
             added[s[i] - 'a'] = true;
         }
         return res;
+        */
     }
 };
