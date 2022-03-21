ec6c7df4935a77156f97d37c847af8028c8df331
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-03-21 20:48:48 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (64.08%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        // https://leetcode.com/problems/partition-labels/discuss/1868842/JavaC%2B%2B-VISUALLY-EXPLAINEDDDDD!!
+        // BEST EXPLANATION :D
+        unordered_map<char,int>mp;
+        // filling impact of character's
+        for(int i = 0; i < s.size(); i++){
+            char ch = s[i];
+            mp[ch] = i;
+        }
+        // making of result
+        vector<int> res;
+        int prev = -1;
+        int maxi = 0;
+        
+        for(int i = 0; i < s.size(); i++){
+            maxi = max(maxi, mp[s[i]]);
+            if(maxi == i){
+                // partition time
+                res.push_back(maxi - prev);
+                prev = maxi;
+            }
+        }
+        return res;
+    }
+};
