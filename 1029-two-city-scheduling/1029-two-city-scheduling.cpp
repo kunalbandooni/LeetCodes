3623a4cef842a72f3d62dec1294704be15d8484e
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-03-25 10:09:43 +0530

    Time: 4 ms (78.75%), Space: 7.8 MB (78.03%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -4,9 +4,6 @@ class Solution {
     }
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
-     /*   sort(costs.begin(),costs.end(),[](vector<int> &v1, vector<int> &v2) {
-    return (v1[0] - v1[1] < v2[0] - v2[1]);
-  });*/
         sort(costs.begin(),costs.end(),cmp);
         int sum=0;
         for(int i=0;i<costs.size()/2;i++)
