8b4d04a916c5f1b19298b04b35cc57381c818149
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-03-26 18:49:53 +0530

    Time: 40 ms (78.79%), Space: 27.6 MB (13.43%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -3,7 +3,9 @@ public:
     int search(vector<int>& nums, int target) {
         int l=0,h=nums.size()-1;
         while(l<=h){
-            int mid=l+(h-l)/2;
+            int mid=l+((h-l)>>1);
+            // mid=l+(h-l)/2;
+            // as mid=(l+h)/2; can overflow :O
             if(nums[mid]==target)
                 return mid;
             if(nums[mid]<target)
