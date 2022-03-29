94f37f79b10c1b55938c096ad2a00e97a127dc8c
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-03-29 19:00:10 +0530

    Time: 201 ms (32.24%), Space: 61.1 MB (91.55%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -2,12 +2,33 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         int n=nums.size();
+        
+        /*
+        Since the numbers are [1:N], so we use the array indices for storing the
+        visited state of each number, only the duplicate will be visited twice.
+        For each number we goto its index position and multiply by -1. In case
+        of duplicate it will be multiplied twice and the number will be +ve.
+        */
+        // TC: O(n)     SC:O(1)
+        for(int i = 0; i < nums.size(); i++) {
+            int index = abs(nums[i]) - 1;
+            
+            // mark as visited
+            nums[index] *= -1;
+            // incase of duplicate, this will become +ve 
+            if(nums[index] > 0)
+                return abs(nums[i]);
+        }    
+        
+        /*
+        TC:O(n)        SC: O(n)
         map<int,int> m;
         for(int i=0;i<n;i++){
             m[nums[i]]++;
             if(m[nums[i]]>1)
                 return nums[i];
         }
+        */
         
         /*
         TC:O(nlogn)
