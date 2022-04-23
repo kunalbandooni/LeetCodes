f218679f1d79087d84de4842d01d171176fdc089
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-04-23 08:32:11 +0530

    Time: 4 ms (70.07%), Space: 7.5 MB (12.86%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,4 +1,29 @@
 class Solution {
+    // https://leetcode.com/problems/encode-and-decode-tinyurl/discuss/1110551/JS-Python-Java-C%2B%2B-or-Easy-Map-Solution-w-Explanation
+    unordered_map<string, string> codeDB, urlDB;
+    const string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    
+    string getCode() {
+        string code = "";
+        for (int i = 0; i < 6; i++) code += chars[rand() % 62];
+        return "http://tinyurl.com/" + code;
+    }
+    
+public:
+    string encode(string longUrl) {
+        if (urlDB.find(longUrl) != urlDB.end()) return urlDB[longUrl];
+        string code = getCode();
+        while (codeDB.find(code) != codeDB.end()) code = getCode();
+        codeDB[code] = longUrl;
+        urlDB[longUrl] = code;
+        return code;
+    }
+    string decode(string shortUrl) {
+        return codeDB[shortUrl];
+    }
+};
+
+/*class Solution {
 public:
 
     // Encodes a URL to a shortened URL.
@@ -11,6 +36,7 @@ public:
         return shortUrl;
     }
 };
+*/
 
 // Your Solution object will be instantiated and called as such:
 // Solution solution;
