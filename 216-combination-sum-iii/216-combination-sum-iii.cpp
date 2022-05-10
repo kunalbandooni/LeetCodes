c73cb91486707e21736974a8e1e9bf66ed029050
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-05-10 19:11:51 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (83.34%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    void backtrack(vector<vector<int>> &result, vector<int> &path, int start, int k, int target){
+        if(target==0 and k==0)
+            result.push_back(path);
+        else
+            for(int i=start;i<=10-k&&i<=target;i++){
+                path.push_back(i);
+                backtrack(result,path,i+1,k-1,target-i);
+                path.pop_back();
+            }
+    }
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> result;
+        vector<int> path;
+        backtrack(result, path, 1, k, n);
+        return result;
+    }
+};
