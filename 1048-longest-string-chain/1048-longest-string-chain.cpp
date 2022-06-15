6658f58f76bcf755e3e9925896e7d66f6979ebad
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-06-15 21:35:03 +0530

    Time: 134 ms (54.53%), Space: 31.3 MB (16.57%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    // https://leetcode.com/problems/longest-string-chain/discuss/1213855/JS-Python-Java-C%2B%2B-or-Fast-Set-DP-Map-Solution-w-Explanation
+public:
+    int longestStrChain(vector<string>& words) {
+        vector<unordered_set<string>> W(17);
+        for (auto word : words) 
+            W[word.size()].insert(word);
+        unordered_map<string, int> dp;
+        int best = 1;
+        for (int i = 16; i; i--) {
+            if (W[i-1].empty()) continue;
+            for (auto word : W[i]) {
+                int wVal = dp[word] ? dp[word] : 1;
+                for (int j = 0; j < word.size(); j++) {
+                    string pred = word.substr(0,j) + word.substr(j+1);
+                    int pVal = dp[pred] ? dp[pred] : 1;
+                    if (W[i-1].find(pred) != W[i-1].end() && wVal >= pVal) {
+                        dp[pred] = wVal + 1;
+                        best = max(best, wVal + 1);
+                    }
+                }
+            }
+        }
+        return best;
+    }
+};
