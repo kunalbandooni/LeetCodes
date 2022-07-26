ee354198bc436d8cfc793b3893206f6321ea3302
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-07-27 05:21:04 +0530

    Time: 21 ms (26.96%), Space: 10.1 MB (71.39%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,20 +1,19 @@
 class Solution {
+    static int count_1(int n){
+        int c = 0;
+        
+        while(n){
+            c += n%2;
+            n=n/2;
+        }
+        
+        return c;
+    }
     static bool func(int a, int b){
-        int n=a,m=b;
-        int c1 = 0;
-        while(a){
-            c1 += a%2;
-            a=a/2;
-        }
-        
-        int c2 = 0;
-        while(b){
-            c2 += b%2;
-            b=b/2;
-        }
-        
+        int c1 = count_1(a);
+        int c2 = count_1(b);        
         if(c1 == c2)
-            return n < m;
+            return a < b;
         
         return c1 < c2;
     }
