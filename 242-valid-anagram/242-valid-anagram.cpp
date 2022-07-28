ca722a8ab274e690c85728cb61743956002fcab5
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-07-28 12:46:22 +0530

    Time: 20 ms (32.51%), Space: 7.4 MB (7.79%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+            return false;
+        
+        map<char,int> m;
+        for(int i=0;i<s.length();i++){
+            m[s[i]]++;
+            m[t[i]]--;
+        }
+        
+        for(auto i:m)
+            if(i.second != 0)
+                return false;
+        return true;
+    }
+};
