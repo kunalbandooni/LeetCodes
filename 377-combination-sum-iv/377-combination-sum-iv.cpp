1dda953482c9aa20130f38f5e97a1b5bc56ac2e6
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-08-05 08:33:54 +0530

    Time: 4 ms (46.34%), Space: 6.8 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -14,12 +14,16 @@ public:
     int combinationSum4(vector<int>& nums, int target) {
         //vector<int> dp(target + 1, -1);
         //return f(target, nums, dp);
+        
+        sort(nums.begin(), nums.end());
+        
         vector<unsigned int> dp(target + 1); 
         dp[0] = 1;
 	    for (int curTarget = 1; curTarget <= target; curTarget++) 
             for (auto& num : nums)     
                 if (num <= curTarget)  
 				    dp[curTarget] += dp[curTarget - num];   
+                else    break;
         return dp[target];
     }
 };
