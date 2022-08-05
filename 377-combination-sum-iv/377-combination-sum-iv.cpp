6089ebba60af5a43d5f8985cb755d7b2df60d323
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-08-05 08:33:02 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int f(int target, vector<int> a, vector<int> &dp){
+    /*int f(int target, vector<int> a, vector<int> &dp){
         if(target == 0) 
             return 1;
         if(dp[target]!=-1)  return dp[target];
@@ -9,10 +9,17 @@ class Solution {
                 res += f(target - a[i], a, dp);
             }
         return dp[target] = res;
-    }
+    }*/
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target + 1, -1);
-        return f(target, nums, dp);
+        //vector<int> dp(target + 1, -1);
+        //return f(target, nums, dp);
+        vector<unsigned int> dp(target + 1); 
+        dp[0] = 1;
+	    for (int curTarget = 1; curTarget <= target; curTarget++) 
+            for (auto& num : nums)     
+                if (num <= curTarget)  
+				    dp[curTarget] += dp[curTarget - num];   
+        return dp[target];
     }
 };
