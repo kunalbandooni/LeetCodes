ba531ac1d3ef65436c1199f8a124c59522907277
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-08-11 08:47:52 +0530

    Time: 12 ms (87.21%), Space: 21.4 MB (98.63%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -10,23 +10,20 @@
  * };
  */
 class Solution {
+    TreeNode* prev = NULL;
 public:
     bool isValidBST(TreeNode* root) {
-        long pre = LONG_MIN;
-        stack<TreeNode*> todo;
-        while (root || !todo.empty()) {
-            while (root) {
-                todo.push(root);
-                root = root -> left;
-            }
-            root = todo.top();
-            todo.pop();
-            if (root -> val <= pre) {
-                return false;
-            }
-            pre = root -> val;
-            root = root -> right;
-        }
+        if(!root) return true;
+        
+        if(!isValidBST(root->left))
+            return false;
+        
+        if(prev && prev->val >= root->val) return false;
+        prev = root;
+        
+        if(!isValidBST(root->right))
+            return false;
+        
         return true;
     }
 };
