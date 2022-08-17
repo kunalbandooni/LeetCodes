1ee145b1a9ad641315a5e1d0fa34868ad563fa01
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-08-17 10:28:07 +0530

    Time: 84 ms (14.81%), Space: 12.4 MB (55.20%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -10,18 +10,18 @@ class Solution {
     }
     public:
     int numIslands(vector<vector<char>>& grid) {
-        int c = 0;
+        int count_of_islands = 0;
         int n = grid.size(), m = grid[0].size();
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == '1'){
                     dfs(i, j, n, m, grid);
-                    c++;
+                    count_of_islands++;
                 }
             }
         }
         
-        return c;
+        return count_of_islands;
     }
 };
