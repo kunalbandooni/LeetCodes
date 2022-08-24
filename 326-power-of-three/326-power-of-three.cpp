498f9a7f5a08d340782189e2f8edabca02ecf20d
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-08-24 07:07:17 +0530

    Time: 33 ms (53.20%), Space: 6.1 MB (7.72%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,4 +1,14 @@
 class Solution {
+public:
+    // if the result of the division of log10(x) by log10(y) is an integer without float part, it means x is a power of y. So we just apply this principle
+    bool isPowerOfThree(int n) {
+        if(n<1) return false;
+        double sol = log10(n) / log10(3);
+        return (sol == (int)sol);
+    }
+    
+    /*
+    // Iterative
     bool func(int n){
         while(n and n%3==0){
             n = n / 3;
@@ -10,4 +20,5 @@ public:
         if(n<1) return false;
         return func(n);
     }
+    */
 };
