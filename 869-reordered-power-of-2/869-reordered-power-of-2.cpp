0b69ba842a59d932711fbcb08cda8e9c75b66209
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-08-26 20:38:55 +0530

    Time: 7 ms (42.94%), Space: 6.6 MB (24.29%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        vector<string> power;
+        for(int i=0;i<=30;i++){
+            int p = pow(2,i);
+            power.push_back(to_string(p));
+        }
+        
+        for(int i=0;i<=30;i++)
+            sort(power[i].begin(),power[i].end());
+        
+        
+        // solution starts here
+        string s = to_string(n);
+        sort(s.begin(),s.end());
+        
+        for(int i=0;i<=30;i++)
+            if(power[i] == s ) 
+                return true;
+        
+        return false;
+    }
+};
