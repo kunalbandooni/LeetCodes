d9608ff2fe175d5922fa80cddb26414b3851c144
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-10-20 16:53:53 +0530

    Time: 23 ms (30.73%), Space: 6 MB (47.60%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string intToRoman(int num) {
+        /*
         int normal[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
         string roman[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         string res = "";
@@ -14,6 +15,14 @@ public:
         
         }
         
-        return res;
+        return res;*/
+        
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string thns[] = {"","M","MM","MMM"};
+    
+        return thns[num/1000] + hrns[(num%1000)/100] + 
+                tens[(num%100)/10] + ones[num%10];
     }
 };
