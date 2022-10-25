4aad3cc34800d484b38bcc5372c94ead0bb9eb21
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-10-25 08:37:27 +0530

    Time: 21 ms (5.57%), Space: 11.3 MB (97.08%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+    /*bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
         string s1 = "", s2 = "";
         
         for(auto i:word1)
@@ -10,6 +10,24 @@ public:
         
         if(s1 == s2)
             return true;
-        return false;
+        return false;*/
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) 
+    {
+        int wordIdx1 = 0, wordIdx2 = 0, chIdx1 = 0, chIdx2 = 0;
+        while(true)
+        {
+            char ch1 = word1[wordIdx1][chIdx1];
+            char ch2 = word2[wordIdx2][chIdx2];
+            if (ch1 != ch2) return false;
+            
+            chIdx1++;
+            chIdx2++;
+            if (chIdx1 == word1[wordIdx1].size()) { wordIdx1++; chIdx1 = 0; }
+            if (chIdx2 == word2[wordIdx2].size()) { wordIdx2++; chIdx2 = 0; }
+            
+            if (wordIdx1 == word1.size() && wordIdx2 == word2.size()) break;
+            if (wordIdx1 == word1.size() || wordIdx2 == word2.size()) return false;
+        }
+        return true; 
     }
 };
