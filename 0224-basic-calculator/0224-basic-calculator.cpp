44dee3465d900fad19b53473dfd85edfd8031714
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-11-20 13:48:28 +0530

    Time: 14 ms (76.16%), Space: 8.2 MB (43.51%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    int evaluate(string& s, int& i) {
+        int res = 0;
+        bool negFlag=false;
+        while(i<s.size()&&s[i]!=')') {
+            if(s[i]=='+'||s[i]==' ') 
+                i++;
+            else if(s[i]=='-') {
+                i++;
+                negFlag=true;
+            }
+            else if(s[i]=='(') {
+                i++;
+                res+=negFlag?-evaluate(s,i):evaluate(s,i);
+                negFlag=false;
+            }
+            else {
+                string strNum{""};
+                while (i < s.size() && isdigit(s[i])) 
+                    strNum += s[i++];
+                int num = std::stoi(strNum);
+                res+=negFlag?-num:num;
+                negFlag=false;
+            }
+        }
+        i++; // skip the current ')'
+        return res;
+    }
+public:
+    int calculate(string s) {
+        int pos=0;
+        return evaluate(s,pos);
+    }
+};
