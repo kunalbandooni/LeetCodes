fc0f1687b98f7c7a101f6300fb1dff7336c2b5ab
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-11-22 17:47:51 +0530

    Time: 394 ms (50.85%), Space: 9 MB (69.87%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    // was trying naive approach, till i found this explanation...
+    // Best solution :
+    //      https://leetcode.com/problems/perfect-squares/discuss/1520447/c%2B%2B-dp-easy-to-understand
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1, INT_MAX);
+        //base case
+        dp[0] = 0;
+        int count = 1;
+        while(count*count <= n) {
+            int sq = count * count;
+            for(int i = sq; i < n + 1; i++)
+                dp[i] = min(dp[i-sq] + 1, dp[i]);
+            count++;
+        }
+        return dp[n];
+    }
+};
