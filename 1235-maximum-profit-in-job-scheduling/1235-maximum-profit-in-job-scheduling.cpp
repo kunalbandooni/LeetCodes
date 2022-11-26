5c634b6ac71c9e97a8244aa9a1ca3a84ed342664
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-11-26 14:04:51 +0530

    Time: 502 ms (16.56%), Space: 58.5 MB (60.39%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    int latest_non_ov_opt(vector<vector<int>> &v,int i){
+        int lo = 0, hi = i - 1; 
+        while (lo <= hi) { 
+            int mid = (lo + hi) / 2; 
+            if (v[mid][0] <= v[i][1]){ 
+                if (v[mid + 1][0] <= v[i][1]) 
+                    lo = mid + 1; 
+                else
+                    return mid; 
+            } 
+            else
+                hi = mid - 1; 
+        } 
+        return -1; 
+    }
+    int rec_mem(vector<vector<int>> &v,int n,vector<int> &mem){
+        if(n==1)
+            return v[n-1][2];
+        if(mem[n]!=-1)
+            return mem[n];
+        int exclusive = rec_mem(v,n-1,mem);
+        int inclusive = v[n-1][2];
+        int i = latest_non_ov_opt(v,n-1);
+        if(i!=-1)
+            inclusive += rec_mem(v,i+1,mem);
+        return mem[n] = max(inclusive,exclusive);
+    }
+public:
+    //call above functions accordingly 
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+        vector<vector<int>>v(n,vector<int>(3));
+        for(int i=0;i<n;i++)
+            v[i] = {endTime[i],startTime[i],profit[i]};
+        sort(v.begin(),v.end());
+        vector<int> mem(n+1,-1);
+        return rec_mem(v,n,mem);
+	}
+};
